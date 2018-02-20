99c2b497ea7f0c109c7b9b5f7e6b8e3d2f2c8303
yannikschaelte <yannik.schaelte@gmail.com>
2018-02-20 10:03:56 +0100

    add analysis for rafmekerk

--- Study-ErrorFunction/rafmekerk/main.m
+++ Study-ErrorFunction/rafmekerk/main.m
@@ -1,6 +1,58 @@
-run_rafmekerk(2);
-run_rafmekerk(1);
-run_rafmekerk(1.1);
-run_rafmekerk(1.5);
-run_rafmekerk(1.7);
-run_rafmekerk(3);
+%% Preliminaries
+
+exdir=fileparts(which('run_rafmekerk.m'));
+addpath(fullfile(exdir,'models'));
+addpath(fullfile(exdir,'data'));
+
+%% Optimization
+
+shapes = {1,1.1,1.5,1.7,2,3};
+nShapes = length(shapes);
+% for j = 1:nShapes
+%     run_rafmekerk(shapes{j});
+% end
+
+%% Analyze
+
+load('data_jakstat.mat','D');
+
+amiOptions.rtol = 1e-10;
+amiOptions.atol = 1e-10;
+amiOptions.sensi_meth = 'forward';
+amiOptions.sensi = 0;
+
+for is = 1:nShapes
+    shape = shapes{is};
+    
+    filename = ['results_' num2str(shape) '_standard.mat'];
+    if ~exist(filename,'file')
+        continue;
+    end
+    load(['results_' num2str(shape) '_standard.mat'],'parameters_res');
+    theta = parameters_res.MS.par(:,1);
+    fprintf('logPost %s = %.10f\n',num2str(shape),parameters_res.MS.logPost(1));
+
+    amiData.t = D(1).t;
+    amiData.Y = D(1).Y(:,:,1);
+    amiData.Sigma_Y = nan(size(amiData.Y));
+    amiData.condition = [D(1).k(:); shape; gamma(1/shape)];
+    amiData = amidata(amiData);
+
+    sol = simulate_rafmekerk_gennormal_standard([], theta, [], amiData, amiOptions);
+
+    for iy = 1:size(D(1).Y,2)
+        fig = figure('name',['fit_jakstat_' num2str(shape) '_' num2str(iy)]);
+        plot(D(1).t,sol.y(:,iy));
+        hold on;
+        plot(D(1).t,D(1).Y(:,iy,1),'.');
+        
+        ym = D(1).Y(:,iy,1);
+        y  = sol.y(:,iy);
+        ym_isnan = isnan(ym);
+        y_isnan = isnan(y);
+        ym(ym_isnan | y_isnan) = [];
+        y(ym_isnan | y_isnan) = [];
+        ycorr = corr(ym,y);
+        fprintf('correlation %s = %.10f\n',num2str(shape),ycorr);
+    end
+end
