57fc279b4e07d026986347038c7d5c7ce66e72d0
yannikschaelte <yannik.schaelte@gmail.com>
2018-06-03 11:52:55 +0200

    rewrite gaussian_cd

--- Study-ABC-AdaptiveDistance/gaussian_cd.py
+++ Study-ABC-AdaptiveDistance/gaussian_cd.py
@@ -27,43 +27,40 @@ observation = {'ss1': theta_true + 1, 'ss2': 8}
 # prior distribution
 prior = pyabc.Distribution(theta=pyabc.RV('uniform', 0, 10))
 
+# further variables
+max_nr_populations = 8
 
 # visualization
-def visualize(history):
+def visualize(label, history):
     fig, ax = plt.subplots()
-    for t in range(history.max_t + 1):
-        df, w = history.get_distribution(m=0, t=t)
-        pyabc.visualization.plot_kde_1d(df, w, xmin=0, xmax=10,
-                                        x='theta', ax=ax,
-                                        label="PDF t={}".format(t))
-    ax.axvline(theta_true, color='k', linestyle='dashed')
-    ax.legend()
-    plt.show()
+    t = history.max_t
+    df, w = history.get_distribution(m=0, t=t)
+    pyabc.visualization.plot_kde_1d(df, w, xmin=0, xmax=10,
+                                    x='theta', ax=ax, 
+                                    refval = {'theta': theta_true},
+                                    label="PDF t={}".format(t))
+    plt.savefig(label + "_plt_kde_1d_" + str(t))
 
 
-# # NON-ADAPTIVE
-# distance = pyabc.PNormDistance(p=2)
-# abc = pyabc.ABCSMC(model, prior, distance)
-db_path = "sqlite:///" + os.path.join(tempfile.gettempdir(), "tmp.db")
-# abc.new(db_path, observation)
-# h1 = abc.run(minimum_epsilon=0, max_nr_populations=8)
-# visualize(h1)
-#
-# # ADAPTIVE
-# distance = pyabc.AdaptivePNormDistance(p=2, adaptive=True)
-# abc = pyabc.ABCSMC(model, prior, distance)
-# abc.new(db_path, observation)
-# h2 = abc.run(minimum_epsilon=0, max_nr_populations=8)
-# visualize(h2)
+def simulate(method):
+    if method == "nada":
+        distance = pyabc.PNormDistance(p=2)
+    elif method == "mad":
+        distance = pyabc.AdaptivePNormDistance(
+                p=2,
+                adaptive=True,
+                scale_function = pyabc.distance_functions.median_absolute_deviation)
+    elif method == "cmad":
+        distance = pyabc.AdaptivePNormDistance(
+                p=2,
+                adaptive=True,
+                scale_function = pyabc.distance_functions.centered_median_absolute_deviation)
 
-# CENTERED
-distance = pyabc.AdaptivePNormDistance(
-    p=2,
-    adaptive=True,
-    scale_type=pyabc.AdaptivePNormDistance.SCALE_TYPE_C_MAD)
-abc = pyabc.ABCSMC(model, prior, distance)
-abc.new(db_path, observation)
-h3 = abc.run(minimum_epsilon=0, max_nr_populations=8)
-visualize(h3)
+    abc = pyabc.ABCSMC(model, prior, distance)
+    db_path = "sqlite:///db.db"
+    abc.new(db_path, observation)
+    h = abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
+    visualize(method, h)
 
 
+simulate("nada")
