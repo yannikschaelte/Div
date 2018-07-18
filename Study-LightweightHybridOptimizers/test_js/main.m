081543731d9e460bd33309849f45dda69ff0b75e
yannikschaelte <yannik.schaelte@gmail.com>
2018-07-18 15:09:19 +0200

    improve output

--- Study-LightweightHybridOptimizers/test_js/main.m
+++ Study-LightweightHybridOptimizers/test_js/main.m
@@ -1,6 +1,8 @@
 clear all;
 
 for j = 0:49
+    disp(['Random seed ' num2str(j)]);
+    
     test_js('latin hypercube', j);
     test_js('ss latinHypercube separatedLHParameters', j);
     test_js('ss latinHypercube clusteredParameters', j);
--- Study-LightweightHybridOptimizers/test_rme/main.m
+++ Study-LightweightHybridOptimizers/test_rme/main.m
@@ -1,6 +1,8 @@
 clear all;
 
 for j = 0:49
+    disp(['Random seed ' num2str(j)]);
+    
     test_rme('latin hypercube', j);
     test_rme('ss latinHypercube separatedLHParameters', j);
     test_rme('ss latinHypercube clusteredParameters', j);
