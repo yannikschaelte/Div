9360f3c7cb3aa9fa2a1b9b7a54cf0b5c7eb1d44c
yannikschaelte <yannik.schaelte@gmail.com>
2018-11-22 16:49:34 +0100

    test

--- pyabc_tests/redis/model.py
+++ pyabc_tests/redis/model.py
@@ -1,12 +1,14 @@
 import numpy as np
 import scipy as sp
 import pyabc
+import time
 
 
-n_t = 50000
+n_t = 10000
 n_r = 1
 
 def model(p):
+    time.sleep(1)
     return {'y' + str(j) : p['mean'] + np.random.randn(n_t) for j in range(0, n_r)}
 
 
@@ -22,6 +24,6 @@ def distance(x, y):
     return np.power(x['y0'] - y['y0'], 2).sum()
 
 
-pop_size = 200
+pop_size = 1000
 redis_sampler = pyabc.sampler.RedisEvalParallelSampler(host="icb-lisa", port=8775)
-multicore_sampler = pyabc.sampler.MulticoreEvalParallelSampler(n_procs=20)
+multicore_sampler = pyabc.sampler.MulticoreEvalParallelSampler(n_procs=40)
