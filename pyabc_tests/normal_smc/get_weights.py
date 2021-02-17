f121a4a9c2e8434435bf065d93b6b18f772738de
yannikschaelte <yannik.schaelte@gmail.com>
2021-02-17 15:12:26 +0100

    whatever

--- pyabc_tests/normal_smc/get_weights.py
+++ pyabc_tests/normal_smc/get_weights.py
@@ -1,12 +1,15 @@
 import pyabc
 import numpy as np
 
-db_file = "sqlite:///db1.db"
-h = pyabc.History(db_file)
-h.id = 1
-
-for t in range(0, h.max_t + 1):
-    d_weighted = h.get_weighted_distances(t)
-    w = np.array(d_weighted['w'])
-    n_eff = np.sum(w)**2 / np.sum(w**2)
-    print(n_eff)
+def get_weights():
+    db_file = "sqlite:///db1.db"
+    h = pyabc.History(db_file)
+    h.id = 1
+    n_effs = []
+    for t in range(0, h.max_t + 1):
+        d_weighted = h.get_weighted_distances(t)
+        w = np.array(d_weighted['w'])
+        n_eff = np.sum(w)**2 / np.sum(w**2)
+        n_effs.append(n_eff)
+        print(n_eff)
+    return n_effs
--- pyabc_tests/normal_smc/test6_viz.py
+++ pyabc_tests/normal_smc/test6_viz.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 import pyabc
 import numpy as np
 import matplotlib
+from get_weights import get_weights
 
 matplotlib.rcParams.update({'font.size': 14})
         
 h = pyabc.History("sqlite:///db1.db")
 h.id = 1
 s2 = np.asarray(h.get_all_populations()['samples'][1:])
+n_effs = get_weights()
+s2 = [np.floor(val * 1000 / n_eff) for val, n_eff in zip(s2, n_effs)]
+print("s2: ", s2)
 s1 = np.zeros_like(s2)
 epsilons = np.array(h.get_all_populations()['epsilon'][1:])
 # plot epsilons
@@ -16,6 +20,7 @@ plt.xlabel("Iteration")
 plt.ylabel("Epsilon")
 plt.tight_layout()
 plt.savefig("eps.png")
+plt.savefig("eps.svg", format="svg")
 plt.close()
 
 plt.figure()
@@ -35,3 +40,4 @@ plt.xlabel("Method")
 plt.subplots_adjust(bottom=.15, left=.15)
 plt.tight_layout()
 plt.savefig("bar.png")
+plt.savefig("bar.svg", format="svg")
